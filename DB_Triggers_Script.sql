9b30e371f738277a5776ae317f518ea69d1d179a
Amanda Hoheisel <131724287+TitanCat@users.noreply.github.com>
2024-12-11 23:25:43 -0600

    Update DB_Triggers_Script.sql

--- DB_Triggers_Script.sql
+++ DB_Triggers_Script.sql
@@ -1,1 +1,25 @@
 -- Triggers Script
+
+-- Updates LocationSafetyLevel based on current LocationPopulation
+CREATE TRIGGER LocationSafetyLevel
+ON [DoomsdayPrepDB].[dbo].[Locations]
+INSTEAD OF UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE L
+    SET 
+        LocationPopulation = I.LocationPopulation, 
+        LocationSafetyLevel = 
+            CASE 
+                WHEN I.LocationPopulation >= 10 THEN 'High'
+                WHEN I.LocationPopulation BETWEEN 5 AND 9 THEN 'Medium'
+                ELSE 'Low'
+            END
+    FROM [DoomsdayPrepDB].[dbo].[Locations] L
+    INNER JOIN INSERTED I
+        ON L.LocationID = I.LocationID;
+END;
+
+
+
