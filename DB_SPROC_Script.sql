42426e3d63dc26df6e09784ada45340f7a4f24e1
BirdBrainlet <armynate12@gmail.com>
2024-12-14 10:38:48 -0600

    Added Ahmed's Stored Procedures

--- DB_SPROC_Script.sql
+++ DB_SPROC_Script.sql
@@ -1,1 +1,44 @@
 -- Stored Procedure Script files
+CREATE PROCEDURE DeleteAdversary
+    @AdversaryID CHAR(4)
+AS
+BEGIN
+    DELETE FROM Adversaries
+    WHERE AdversaryID = @AdversaryID;
+END;
+GO
+
+CREATE PROCEDURE UpdateInventoryQuantity
+    @InventoryID CHAR(4),
+    @NewQuantity INT
+AS
+BEGIN
+    UPDATE Inventory
+    SET Quantity = @NewQuantity
+    WHERE InventoryID = @InventoryID;
+END;
+GO
+
+CREATE PROCEDURE AddNewAlly
+    @AllyID CHAR(4),
+    @LocationID CHAR(4),
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @TrustLevel NVARCHAR(6)
+AS
+BEGIN
+    INSERT INTO Allies (AllyID, LocationID, AllyFirstName, AllyLastName, TrustLevel)
+    VALUES (@AllyID, @LocationID, @FirstName, @LastName, @TrustLevel);
+END;
+GO
+
+CREATE PROCEDURE GetPeopleByLodging
+    @LodgingID CHAR(4)
+AS
+BEGIN
+    SELECT P.PeopleID, P.PeopleFirstName, P.PeopleLastName, P.Age
+    FROM People P
+    INNER JOIN PeopleLodging PL ON P.PeopleID = PL.PeopleID
+    WHERE PL.LodgingID = @LodgingID;
+END;
+GO
